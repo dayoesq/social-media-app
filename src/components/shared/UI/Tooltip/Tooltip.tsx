43bbdf1a72a0d2931c9d71449e72132865354062
Dayo Oladapo <dayoesq@live.com>
2022-01-06 14:21:52 +0200

    Delete post, image optimization...

--- src/components/shared/UI/Tooltip/Tooltip.tsx
+++ src/components/shared/UI/Tooltip/Tooltip.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faSave, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faSave, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-import classes from './Tooltip.module.scss';
+import classes from "./Tooltip.module.scss";
 
 interface ITooltip {
   style?: React.CSSProperties;
   onDelete?: React.MouseEventHandler<HTMLDivElement>;
   onHide?: React.MouseEventHandler<HTMLDivElement>;
   onEdit?: React.MouseEventHandler<HTMLDivElement>;
-  text?: string;
 }
 
-const Tooltip: React.FC<ITooltip> = props => {
+const Tooltip: React.FC<ITooltip> = (props) => {
+  const { style, onDelete, onHide, onEdit } = props;
   return (
-    <div className={classes.tooltip} style={props.style}>
-      <div onClick={props.onDelete}>
-        <FontAwesomeIcon icon={faTrashAlt} size='1x' color='#9e9a9a' />
-        <p>Delete {props.text}</p>
+    <div className={classes.tooltip} style={style}>
+      <div onClick={onDelete}>
+        <FontAwesomeIcon icon={faTrashAlt} size="1x" color="#9e9a9a" />
+        <p>Delete</p>
       </div>
-      <div onClick={props.onHide}>
-        <FontAwesomeIcon icon={faSave} size='1x' color='#9e9a9a' />
-        <p>Hide {props.text}</p>
+      <div onClick={onHide}>
+        <FontAwesomeIcon icon={faSave} size="1x" color="#9e9a9a" />
+        <p>Hide</p>
       </div>
-      <div onClick={props.onEdit}>
-        <FontAwesomeIcon icon={faEdit} size='1x' color='#9e9a9a' />
-        <p>Edit {props.text}</p>
+      <div onClick={onEdit}>
+        <FontAwesomeIcon icon={faEdit} size="1x" color="#9e9a9a" />
+        <p>Edit</p>
       </div>
     </div>
   );
@@ -36,8 +36,7 @@ Tooltip.propTypes = {
   onDelete: PropTypes.func,
   onHide: PropTypes.func,
   onEdit: PropTypes.func,
-  text: PropTypes.string,
-  style: PropTypes.object
+  style: PropTypes.object,
 };
 
 export default Tooltip;
